d61aed9731468362aea8df12255f4add586354ec
zhanglinghua123 <1441749449@qq.com>
2022-05-26 11:07:54 +0800

    add jest

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import dts from "rollup-plugin-dts";
-
+import { terser } from "rollup-plugin-terser"
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 const packageJson = require("./package.json");
 
 export default [
@@ -22,10 +23,12 @@ export default [
             },
         ],
         plugins: [
+            peerDepsExternal(),
             resolve(),
             commonjs(),
             typescript({ tsconfig: "./tsconfig.json" }),
             postcss(),
+            terser(),
         ],
     },
     {
