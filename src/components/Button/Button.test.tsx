5a3dbae02b7dd6ce4629ebaf5966296a603c5ab2
zhanglinghua123 <1441749449@qq.com>
2022-05-26 11:26:55 +0800

    sasa

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import renderer from 'react-test-renderer';
 
 import Button from "./Button";
 
 describe("Button", () => {
     test("renders the Button component", () => {
-        render(<Button label="Hello world!" />);
+        renderer.create(<Button label="Hello world!" />);
     });
 });
