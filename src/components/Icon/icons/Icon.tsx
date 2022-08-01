3941f866bc92a905392343771768ed4d6fea4536
zhanglinghua123 <1441749449@qq.com>
2022-08-02 00:16:27 +0800

    feat:增加ICON onclick

--- src/components/Icon/icons/Icon.tsx
+++ src/components/Icon/icons/Icon.tsx
@@ -6,12 +6,14 @@ export type IconProps = {
     src?: string;
     size?: string;
     style?: CSSProperties;
+    onClick?:(e:any)=>void
 };
 export const Icon = (props: IconProps) => {
-    const { src, size = '1em' ,style} = props;
+    const { src, size = '1em' ,style,onClick} = props;
     let imgSrc = StringMapIntoSVG(src);
     return (
         <img
+            onClick={onClick}
             src={imgSrc}
             style={{ width: size, height: size, ...style}}
             className={'my-prefix-icon-img'}
